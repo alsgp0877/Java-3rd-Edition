b6c694333db3a0b3fc0983149bb02af7dd245ec3
alsgp0877 <alsgp0877@naver.com>
2022-05-15 23:50:25 +0900

    chapter02 형변환 조금 ~ chapter03 3.2 나머지 연산자까지

---
+++ src/Practice/예제코딩.java
@@ -0,0 +1,46 @@
+package Practice;
+
+public class 예제코딩 {
+
+	/*
+	 * public static void main(String[] args) { double d = 85.4; int score=(int)d;
+	 * 
+	 * System.out.println("score="+score); System.out.println("d="+d);
+	 * 
+	 * }
+	 */
+	
+	/*
+	 * public static void main(String[] args) { int i =5; i++;
+	 * System.out.println(i); i=5; ++i; System.out.println(i);
+	 * 
+	 * }
+	 */
+	
+	/*
+	 * public static void main(String[] args) { int i =5,j=0; j=i++;
+	 * System.out.println("j=i++;실행 후, i="+i+",j="+j); i=5; j=0;
+	 * 
+	 * j=++i; System.out.println("j=++i;실행 후, i="+i+",j="+j);
+	 * 
+	 * }
+	 */
+	
+	/*
+	 * public static void main(String[] args) { char c1 ='a'; char c2 =c1+1; char c3
+	 * ='a'+1;
+	 * 
+	 * System.out.println(c2);
+	 * 
+	 * }
+	 */
+	
+	public static void main(String[] args) {
+		char lc ='a';
+		char uc =(char)(lc-32);
+		
+		
+		System.out.println(uc);
+
+	}
+}
