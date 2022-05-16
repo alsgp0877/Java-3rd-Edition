2aaeaf4009ad2add611556da8ebd38d7cfd65b82
alsgp0877 <alsgp0877@naver.com>
2022-05-17 00:09:24 +0900

    chapter03 연산자 완료!

--- src/Practice/예제코딩.java
+++ src/Practice/예제코딩.java
@@ -35,12 +35,26 @@ public class 예제코딩 {
 	 * }
 	 */
 	
+	/*
+	 * public static void main(String[] args) { char lc ='a'; char uc
+	 * =(char)(lc-32);
+	 * 
+	 * 
+	 * System.out.println(uc);
+	 * 
+	 * }
+	 */
 	public static void main(String[] args) {
-		char lc ='a';
-		char uc =(char)(lc-32);
-		
-		
-		System.out.println(uc);
+		int a=5;
+		int b=0;
+		System.out.printf("a=%d,b=%d%n",a,b);
+		/*
+		 * System.out.printf("a!=0 || ++b!=0 = %b%n", a!=0 || ++b!=0);
+		 * System.out.printf("++b!=0 = %b%n", ++b!=0);
+		 */
+		System.out.printf("%d%n", ++b);
+		System.out.printf("%d%n", b);
+
 
 	}
 }
