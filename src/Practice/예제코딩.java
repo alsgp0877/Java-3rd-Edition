1a5b8cad3637178874f1cc6af92fd1fd82789d1b
alsgp0877 <alsgp0877@naver.com>
2022-06-09 00:07:14 +0900

    조건문switch 끝, 반복문 공부중

--- src/Practice/예제코딩.java
+++ src/Practice/예제코딩.java
@@ -1,5 +1,7 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class 예제코딩 {
 
 	/*
@@ -9,14 +11,14 @@ public class 예제코딩 {
 	 * 
 	 * }
 	 */
-	
+
 	/*
 	 * public static void main(String[] args) { int i =5; i++;
 	 * System.out.println(i); i=5; ++i; System.out.println(i);
 	 * 
 	 * }
 	 */
-	
+
 	/*
 	 * public static void main(String[] args) { int i =5,j=0; j=i++;
 	 * System.out.println("j=i++;실행 후, i="+i+",j="+j); i=5; j=0;
@@ -25,7 +27,7 @@ public class 예제코딩 {
 	 * 
 	 * }
 	 */
-	
+
 	/*
 	 * public static void main(String[] args) { char c1 ='a'; char c2 =c1+1; char c3
 	 * ='a'+1;
@@ -34,7 +36,7 @@ public class 예제코딩 {
 	 * 
 	 * }
 	 */
-	
+
 	/*
 	 * public static void main(String[] args) { char lc ='a'; char uc
 	 * =(char)(lc-32);
@@ -44,16 +46,40 @@ public class 예제코딩 {
 	 * 
 	 * }
 	 */
+	/*
+	 * public static void main(String[] args) { int a=5; int b=0;
+	 * System.out.printf("a=%d,b=%d%n",a,b);
+	 * 
+	 * System.out.printf("a!=0 || ++b!=0 = %b%n", a!=0 || ++b!=0);
+	 * System.out.printf("++b!=0 = %b%n", ++b!=0);
+	 * 
+	 * System.out.printf("%d%n", ++b); System.out.printf("%d%n", b);
+	 * 
+	 * 
+	 * }
+	 */
+	//예제4-7
 	public static void main(String[] args) {
-		int a=5;
-		int b=0;
-		System.out.printf("a=%d,b=%d%n",a,b);
-		/*
-		 * System.out.printf("a!=0 || ++b!=0 = %b%n", a!=0 || ++b!=0);
-		 * System.out.printf("++b!=0 = %b%n", ++b!=0);
-		 */
-		System.out.printf("%d%n", ++b);
-		System.out.printf("%d%n", b);
+		System.out.print("가위(1),바위(2),보(3) 중 하나를 입력하세요>");
+		Scanner scanner = new Scanner(System.in);
+		
+		int user = scanner.nextInt();
+		int com = (int)(Math.random()*3)+1;
+		
+		System.out.print("당신은"+user+"입니다.");
+		System.out.print("컴은"+com+"입니다.");
+		
+		switch(user-com) {
+			case 2: case -1:
+				System.out.print("당신이졌습니다.");
+				break;
+			case 1: case -2:
+				System.out.print("당신이이겼습니다.");
+				break;
+			case 0:
+				System.out.print("비겼습니다.");
+				break;
+		}
 
 
 	}
