00355a19b35ec370e4e782019a7b4f0ce0b8c83b
alsgp0877 <alsgp0877@naver.com>
2022-06-09 23:38:36 +0900

    chapter04 조건문 while문까지

--- src/Practice/예제코딩.java
+++ src/Practice/예제코딩.java
@@ -58,29 +58,44 @@ public class 예제코딩 {
 	 * 
 	 * }
 	 */
-	//예제4-7
+	/*
+	 * //예제4-7 public static void main(String[] args) {
+	 * System.out.print("가위(1),바위(2),보(3) 중 하나를 입력하세요>"); Scanner scanner = new
+	 * Scanner(System.in);
+	 * 
+	 * int user = scanner.nextInt(); int com = (int)(Math.random()*3)+1;
+	 * 
+	 * System.out.print("당신은"+user+"입니다."); System.out.print("컴은"+com+"입니다.");
+	 * 
+	 * switch(user-com) { case 2: case -1: System.out.print("당신이졌습니다."); break; case
+	 * 1: case -2: System.out.print("당신이이겼습니다."); break; case 0:
+	 * System.out.print("비겼습니다."); break; }
+	 * 
+	 * 
+	 * }
+	 */
+
+	/*
+	 * //예제4-14 public static void main(String[] args) {
+	 * 
+	 * for(int i=1,j=10;i<=10;i++,j--) { System.out.printf("%d\t %d\n",i,j);
+	 * 
+	 * }
+	 * 
+	 * for(int i=1;i<=10;i++) { System.out.printf("%d\t %d\n",i,11-i); } }
+	 */
+	
+	/*
+	 * //예제4-15 public static void main(String[] args) { for(int i=1;i<=10;i++) {
+	 * System.out.printf("%d\t %d\n",i%3,i/3); } }
+	 */
+	
 	public static void main(String[] args) {
-		System.out.print("가위(1),바위(2),보(3) 중 하나를 입력하세요>");
-		Scanner scanner = new Scanner(System.in);
-		
-		int user = scanner.nextInt();
-		int com = (int)(Math.random()*3)+1;
-		
-		System.out.print("당신은"+user+"입니다.");
-		System.out.print("컴은"+com+"입니다.");
-		
-		switch(user-com) {
-			case 2: case -1:
-				System.out.print("당신이졌습니다.");
-				break;
-			case 1: case -2:
-				System.out.print("당신이이겼습니다.");
-				break;
-			case 0:
-				System.out.print("비겼습니다.");
-				break;
+		int i=5;
+		while(--i!=0) {
+			System.out.printf(i+"- i can do it");
+			
 		}
-
-
 	}
 }
+
