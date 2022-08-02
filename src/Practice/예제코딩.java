7cbfa733571a1d1ec3dd55737c2eb0f82116bef5
alsgp0877 <alsgp0877@naver.com>
2022-08-02 22:59:31 +0900

    chaper04 coninue 예제 , 홀수는 맨뒷자리만 계산하면됨

--- src/Practice/예제코딩.java
+++ src/Practice/예제코딩.java
@@ -98,23 +98,76 @@ public class 예제코딩 {
 	 */
 	
 	//4-30 break문
+	/*
+	 * public static void main(String[] args) { int sum=0; int i=0;
+	 * 
+	 * while(true) { if(sum>100) { break; } //--break문이 수행되면 이 부분은 실행되지 않고 while문을
+	 * 완전히 벗어난다. ++i; sum+=i; //--
+	 * 
+	 * }//end of while
+	 * 
+	 * System.out.println("i="+i); System.out.println("sum="+sum);
+	 * 
+	 * }
+	 */
+	//4-29
+	//숫자 중에 3의 배수(3,6,9)가 포함되어있으면, 포함된 개수만큼 박수를 치는 369게임을 1부터 100까지 출력하는 예제이다.
+	/*
+	 * public static void main(String[] args) { for(int i=1;i<=100;i++) {
+	 * System.out.printf("i=%d",i); int tmp = i; do { //tmp%10이 3의 배수인지 확인(0제외)
+	 * if(tmp%10%3==0 && tmp%10!=0) {//2자리안이라는 고정하에 끝자리만 검색하는거구나 그래도 홀수인지 아닌지 알수있으니까
+	 * System.out.print("짝"); } }while((tmp/=10)!=0); } System.out.println();
+	 * 
+	 * }
+	 */
+	
+	//4-34
 	public static void main(String[] args) { 
-		int sum=0;
-		int i=0;
+		int menu = 0;
+		int num = 0;
 		
+		outer:
 		while(true) {
-			if(sum>100) {
+			System.out.println("(1) square");
+			System.out.println("(2) square root");
+			System.out.println("(3) log");
+			System.out.print("원하는 메뉴(1~3)를 선택하세요. (종료:0)>");
+			Scanner scanner = new Scanner(System.in);
+			String tmp = scanner.nextLine();
+			menu = Integer.parseInt(tmp);
+			if(menu==0) {
+				System.out.println("프로그램을 종료합니다.");
 				break;
+			}else if(!(1<=menu&&menu<=3)) {
+				System.out.println("메뉴를 잘못 선택하셨습니다(종료는0) ");
+				continue;
 			}
-			//--break문이 수행되면 이 부분은 실행되지 않고 while문을 완전히 벗어난다.
-			++i;
-			sum+=i;
-			//--
 			
-		}//end of while
-	  
-		System.out.println("i="+i); 
-		System.out.println("sum="+sum);
+			for(;;) {
+				System.out.println("계산할 값을 입력하세요. (계산 종료:0, 전체종료:99)>");
+				tmp= scanner.nextLine();
+				num = Integer.parseInt(tmp);
+				
+				if(num==0) {
+					break;
+				}
+				
+				if(num==99) {
+					break outer;
+				}
+				switch(menu) {
+					case 1:
+						System.out.println("result=" + num*num);
+						break;
+					case 2:
+						System.out.println("result=" + Math.sqrt(num));
+						break;
+					case 3:
+						System.out.println("result=" + Math.log(num));
+						break;
+				}
+			}
+		}
 	  
 	}
 	
